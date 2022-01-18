8dbd47962f10e7edc8aec71b99cd425f40e43f3f
Guilherme Almeida <gui1612@sapo.pt>
2022-01-18 20:00:21 +0000

    feat: adds 7th query

--- queries/int7.sql
+++ queries/int7.sql
@@ -2,3 +2,7 @@
 .headers on
 .nullvalue NULL
 
+SELECT DISTINCT Pr.name as professor, S.name
+FROM Lecturer L, Professor as Pr, Period as Pe, Subject as S
+Where L.Period=Pe.id AND L.professor=Pr.id AND Pe.subject=S.id
+ORDER BY S.name, professor;
