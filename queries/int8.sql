ec9a40f089773975b0314b2a2def92dd7243f170
Guilherme Almeida <gui1612@sapo.pt>
2022-01-19 10:53:13 +0000

    feat: adds queries 8 and 9

--- queries/int8.sql
+++ queries/int8.sql
@@ -2,3 +2,9 @@
 .headers on
 .nullvalue NULL
 
+SELECT S.name as subject, ET.name as type, startDateTime, endDateTime
+FROM EvaluationEvent EE, Subject S, EvaluationType ET
+WHERE EE.subject=S.id AND EE.type=ET.id
+ORDER BY
+    ABS(date('now') - startDateTime) ASC
+LIMIT 1;
--- queries/int9.sql
+++ queries/int9.sql
@@ -2,3 +2,8 @@
 .headers on
 .nullvalue NULL
 
+SELECT S.name as subject, ET.name as type, startDateTime, endDateTime
+FROM EvaluationEvent EE, Subject S, EvaluationType ET
+WHERE EE.subject=S.id AND EE.type=ET.id
+    AND  startDateTime < datetime('now')
+ORDER BY subject, type, startDateTime;
