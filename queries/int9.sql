75fa690bb376fdaeb9e94698c6834f2fb62da067
André Lima <me@limwa.pt>
2022-01-20 12:27:18 +0000

    refactor: format query #9

--- queries/int9.sql
+++ queries/int9.sql
@@ -1,7 +1,9 @@
-SELECT GC.name as name, S.name as subject, ET.name as type,     
+SELECT 
+    GC.name as name,
+    S.name as subject,
+    ET.name as type,
     strftime('%Y-%m-%d %Hh%M', startDateTime) as startDateTime,
     strftime('%Y-%m-%d %Hh%M', endDateTime) as endDateTime
 FROM EvaluationEvent EE, GradeComponent GC, Subject S, EvaluationType ET
-WHERE EE.subject=S.id AND EE.type=ET.id AND GC.id=EE.grade
-    AND endDateTime < datetime('now')
+WHERE EE.subject=S.id AND EE.type=ET.id AND GC.id=EE.grade AND endDateTime < datetime('now')
 ORDER BY endDateTime DESC, startDateTime DESC, subject, type;
