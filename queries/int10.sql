2d0a98bd40653d2c25e02b0d8eae0fc80678f148
André Lima <me@limwa.pt>
2022-01-21 03:09:06 +0000

    feat: jesus fuck

--- queries/int10.sql
+++ queries/int10.sql
@@ -1,26 +1,25 @@
 .read criar.sql
 .read povoar.sql
 
-INSERT INTO GradeComponent
-VALUES (90, '1', NULL, 1, 1, NULL),
-    (91, '2', NULL, 1, 1, NULL),
-    (92, '1L', NULL, 0.15, 1, 90),
-    (93, '1M', NULL, 0.60, 1, 90),
-    (94, '1R', NULL, 0.15, 1, 90),
-    (95, '1LL', NULL, 0.80, 1, 92),
-    (96, '1LR', NULL, 0.20, 1, 92),
-    (97, '2L', NULL, 0.15, 1, 91),
-    (98, '2M', NULL, 0.60, 1, 91),
-    (99, '2R', NULL, 0.15, 1, 91),
-    (100, '2LL', NULL, 0.80, 1, 97),
-    (101, '2LR', NULL, 0.20, 1, 97),
-    (102, '1D', NULL, 0.1, 1, 90),
-    (103, '1DL', NULL, 0.80, 1, 102),
-    (104, '1DR', NULL, 0.20, 1, 102),
-    (105, '2D', NULL, 0.1, 1, 91),
-    (106, '2DL', NULL, 0.80, 1, 105),
-    (107, '2DR', NULL, 0.20, 1, 105)
-    ;
+-- INSERT INTO GradeComponent
+-- VALUES (90, '1', NULL, 1, 1, NULL),
+--     (91, '2', NULL, 1, 1, NULL),
+--     (92, '1L', NULL, 0.15, 1, 90),
+--     (93, '1M', NULL, 0.60, 1, 90),
+--     (94, '1R', NULL, 0.15, 1, 90),
+--     (95, '1LL', NULL, 0.80, 1, 92),
+--     (96, '1LR', NULL, 0.20, 1, 92),
+--     (97, '2L', NULL, 0.15, 1, 91),
+--     (98, '2M', NULL, 0.60, 1, 91),
+--     (99, '2R', NULL, 0.15, 1, 91),
+--     (100, '2LL', NULL, 0.80, 1, 97),
+--     (101, '2LR', NULL, 0.20, 1, 97),
+--     (102, '1D', NULL, 0.1, 1, 90),
+--     (103, '1DL', NULL, 0.80, 1, 102),
+--     (104, '1DR', NULL, 0.20, 1, 102),
+--     (105, '2D', NULL, 0.1, 1, 91),
+--     (106, '2DL', NULL, 0.80, 1, 105),
+--     (107, '2DR', NULL, 0.20, 1, 105);
 
 CREATE VIEW LayersSimilar AS
 SELECT gc1.id AS id1,
@@ -60,61 +59,97 @@ WHERE gc1.id != gc2.id
             )
     );
 
-
--- SELECT
---     gc1.id, gc2.id
---     FROM GradeComponent gc1, GradeComponent gc2 WHERE
-WITH RECURSIVE Similar(id1, id2) AS (
-    SELECT 90, 91 WHERE (90, 91) IN LayersSimilar
-    UNION ALL
-    SELECT c1.id,
-        c2.id
-    FROM Similar,
-        GradeComponent c1,
-        GradeComponent c2
-    WHERE Similar.id1 = c1.parent
-        AND Similar.id2 = c2.parent
-        AND (c1.id, c2.id) IN LayersSimilar
-), Tree1(id) AS (
-    SELECT 90
-    UNION ALL
-    SELECT GradeComponent.id FROM Tree1, GradeComponent WHERE Tree1.id = GradeComponent.parent
-), Tree2(id) AS (
-    SELECT 91
-    UNION ALL
-    SELECT GradeComponent.id FROM Tree2, GradeComponent WHERE Tree2.id = GradeComponent.parent
-)
-SELECT NOT EXISTS (
-        SELECT *
-        FROM (
-                SELECT DISTINCT id1 as id FROM Similar
-                EXCEPT
-                SELECT id FROM Tree1
+SELECT s1.name as subject1, t1.name as term1, s2.name as subject2, t2.name as term2
+FROM Term t1, Term t2, Subject s1, Subject s2, TermGrades tg1, TermGrades tg2, GradeComponent gc1,
+    GradeComponent gc2
+WHERE tg1.grade=gc1.id AND tg1.subject=s1.id AND tg1.term=t1.id 
+    AND tg2.grade=gc2.id AND tg2.subject=s2.id AND tg2.term=t2.id
+AND gc1.id < gc2.id
+    AND gc1.parent IS NULL
+    AND gc2.parent IS NULL
+    AND 1 in (
+        WITH RECURSIVE Similar(id1, id2) AS (
+            SELECT gc1.id,
+                gc2.id
+            WHERE (gc1.id, gc2.id) IN LayersSimilar
+            UNION ALL
+            SELECT c1.id,
+                c2.id
+            FROM Similar,
+                GradeComponent c1,
+                GradeComponent c2
+            WHERE Similar.id1 = c1.parent
+                AND Similar.id2 = c2.parent
+                AND (c1.id, c2.id) IN LayersSimilar
+        ),
+        Tree1(id) AS (
+            SELECT gc1.id
+            UNION ALL
+            SELECT GradeComponent.id
+            FROM Tree1,
+                GradeComponent
+            WHERE Tree1.id = GradeComponent.parent
+        ),
+        Tree2(id) AS (
+            SELECT gc2.id
+            UNION ALL
+            SELECT GradeComponent.id
+            FROM Tree2,
+                GradeComponent
+            WHERE Tree2.id = GradeComponent.parent
+        )
+        SELECT NOT EXISTS (
+                SELECT *
+                FROM (
+                        SELECT DISTINCT id1 as id
+                        FROM Similar
+                        EXCEPT
+                        SELECT id
+                        FROM Tree1
+                    )
+                UNION ALL
+                SELECT *
+                FROM (
+                        SELECT id
+                        FROM Tree1
+                        EXCEPT
+                        SELECT DISTINCT id1 as id
+                        FROM Similar
+                    )
             )
-        UNION ALL
-        SELECT *
-        FROM (
-                SELECT id FROM Tree1
-                EXCEPT
-                SELECT DISTINCT id1 as id FROM Similar
+            AND NOT EXISTS (
+                SELECT *
+                FROM (
+                        SELECT DISTINCT id2 as id
+                        FROM Similar
+                        EXCEPT
+                        SELECT id
+                        FROM Tree2
+                    )
+                UNION ALL
+                SELECT *
+                FROM (
+                        SELECT id
+                        FROM Tree2
+                        EXCEPT
+                        SELECT DISTINCT id2 as id
+                        FROM Similar
+                    )
             )
-) AND NOT EXISTS (
-    SELECT *
-        FROM (
-                SELECT DISTINCT id2 as id FROM Similar
-                EXCEPT
-                SELECT id FROM Tree2
-            )
-        UNION ALL
-        SELECT *
-        FROM (
-                SELECT id FROM Tree2
-                EXCEPT
-                SELECT DISTINCT id2 as id FROM Similar
-            )
-) 
-AND NOT EXISTS (
-    SELECT *, count(*) as nr1 FROM Similar s1, (SELECT id2, count(*) as nr2 FROM Similar GROUP BY id2) s2 WHERE s2.id2=s1.id2 GROUP BY s1.id1 HAVING nr1 != nr2
-) as equals;
+            AND NOT EXISTS (
+                SELECT *,
+                    count(*) as nr1
+                FROM Similar s1,
+                    (
+                        SELECT id2,
+                            count(*) as nr2
+                        FROM Similar
+                        GROUP BY id2
+                    ) s2
+                WHERE s2.id2 = s1.id2
+                GROUP BY s1.id1
+                HAVING nr1 != nr2
+            ) as equals
+    ) ORDER BY t1.startDate DESC, t2.startDate DESC, s1.name ASC, s2.name ASC;
 
 DROP VIEW LayersSimilar;
