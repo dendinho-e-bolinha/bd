34c8514493ef4d046880684bd6dc16e84fc3f9fb
Guilherme Almeida <gui1612@sapo.pt>
2022-01-22 15:15:01 +0000

    fix: changed formating

--- triggers/gatilho3_adiciona.sql
+++ triggers/gatilho3_adiciona.sql
@@ -1,18 +1,14 @@
-CREATE TRIGGER gradeWithinLimits BEFORE
-INSERT
-  ON GradeComponent FOR EACH ROW BEGIN
-SELECT
-  RAISE(ABORT, 'The grade is not within chosen limits')
-WHERE
-  NOT EXISTS (
-    SELECT
-      gradeLimit.upper as upper,
-      gradeLimit.lower as lower
-    FROM
-      GradeLimit
-    WHERE
-      New.gradeLimit = GradeLimit.id
-      AND New.value <= upper
-      AND New.value >= lower
-  );
+CREATE TRIGGER gradeWithinLimits 
+BEFORE INSERT ON GradeComponent 
+FOR EACH ROW 
+BEGIN
+    SELECT RAISE(ABORT, 'The grade is not within chosen limits')
+    WHERE NOT EXISTS (
+            SELECT gradeLimit.upper as upper,
+                gradeLimit.lower as lower
+            FROM GradeLimit
+            WHERE New.gradeLimit = GradeLimit.id
+                AND New.value <= upper
+                AND New.value >= lower
+        );
 END;
